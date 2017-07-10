bf33d2c04cfcd51a2826a087d5d1add48ec97e23
bugzhang <flyhighdream@qq.com>
2017-07-11 02:10:32 +0800

    修改目录结构

--- module/db.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by bugzhang on 2016/11/27.
- */
-var setting = require('../settings'),
-    Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = require('mongodb').Server;
-module.exports = new Db(setting.db,new Server(setting.host,setting.port),{safe:ture});
--- settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    cookieSecret:'myBlog',
-    db:'blog',
-    host:'localhost',
-    port:'27017'
-};
