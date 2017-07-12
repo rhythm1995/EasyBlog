384d2119873532c30dbc4b1701e133f390847ff8
bugzhang <flyhighdream@qq.com>
2017-07-12 13:48:19 +0800

    首页header

---
+++ front-end/src/components/article.vue
@@ -0,0 +1,72 @@
+<style lang="less">
+
+</style>
+
+<template>
+    <article>
+        <section class="post-excerpt">
+            <a href="/2017/07/02/vue-jsti-xi-jie-gou-zong-jie/">
+                <p>
+                    <img src="http://7xqgks.com1.z0.glb.clouddn.com/head-0029.jpg" alt="">
+                </p>
+            </a>
+            <div class="info-mask">
+                <div class="mask-wrapper">
+                    <h2 class="post-title">
+                        <a href="/2017/07/02/vue-jsti-xi-jie-gou-zong-jie/">Vue.js体系结构总结</a>
+                    </h2>
+                    <div class="post-meta">
+                        <span class="post-time">
+                            <time datetime="2017-07-02">02 Jul 2017</time>
+                        </span>
+                        <span class="post-tags">
+                            <a href="/tag/javascript/">javascript</a>
+                            <a href="/tag/qian-duan/">前端</a>
+                            <a href="/tag/web/">Web</a>
+                            <a href="/tag/qian-duan-kuang-jia/">前端框架</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="post-excerpt-mirror">
+            <div class="post-excerpt-mirror-mask">
+                <a href="/2017/07/02/vue-jsti-xi-jie-gou-zong-jie/">
+                    <p>
+                        <img src="http://7xqgks.com1.z0.glb.clouddn.com/head-0029.jpg" alt="">
+                    </p>
+                </a>
+    
+                <div class="excert-detail-container">
+                    <div class="post-meta">
+                        <span class="post-time">
+                            <time datetime="2017-07-02"> 02 Jul 2017 </time>
+                        </span>
+                        <h2 class="post-title">
+                            <a href="/2017/07/02/vue-jsti-xi-jie-gou-zong-jie/">Vue.js体系结构总结</a>
+                        </h2>
+                        <p class="post-short-intro">结束实习后，终于有了很多自己时间学习，最近重新看vue和node.js。 终于算是基本过了一遍vue.js，也写了一个小网站，总结下vue的知识体系结构，温故知新。 之后计划学node和koa框架，</p>
+                        <span class="post-tags">
+                            <a href="/tag/javascript/">javascript</a>
+                            <a href="/tag/qian-duan/">前端</a>
+                            <a href="/tag/web/">Web</a>
+                            <a href="/tag/qian-duan-kuang-jia/">前端框架</a>
+                        </span>
+                        <a href="/2017/07/02/vue-jsti-xi-jie-gou-zong-jie/" class="btn-post-excerpt">阅读原文</a>
+                    </div>
+    
+                </div>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'article',
+    data() {
+
+    }
+}
+</script>
+
---
+++ front-end/src/views/index.vue
@@ -0,0 +1,122 @@
+<style scoped lang="less">
+body {
+    font-family: Exo, '-apple-system', 'Open Sans', HelveticaNeue-Light, 'Helvetica Neue Light', 'Helvetica Neue', 'Hiragino Sans GB', 'Microsoft YaHei', Helvetica, Arial, sans-serif;
+}
+
+header {
+    width: 100%;
+    height: 35em;
+    background: url('../../public/background.jpg') no-repeat;
+    background-position: 0 40%;
+    background-size: cover;
+
+    span {
+        .header-logo-img {
+            margin-left: 10%;
+            margin-top: 1.5em;
+            margin-bottom: 10em;
+            width: 5em;
+            height: 5em;
+            background: url('http://of8t6pu0g.bkt.clouddn.com/head3.jpg') no-repeat;
+            background-position: 0 40%;
+            background-size: cover;
+        }
+    }
+
+    .header-topic {
+        display: block;
+        text-align: center;
+        margin: 0 auto;
+        h1 {
+            font-weight: lighter;
+            color: #fff;
+            text-shadow: 0 1px 1px #595959;
+            font-size: 4em;
+            &:hover {
+                color: #efefef;
+                text-shadow: 0 1px 1px #000;
+            }
+        }
+
+        h4 {
+            font-weight: lighter;
+            color: #fff;
+            text-shadow: 0 1px 1px #595959;
+            font-size: 2em;
+            letter-spacing: .8rem;
+            &:hover {
+                color: #efefef;
+                text-shadow: 0 1px 1px #fff;
+            }
+        }
+        .header-menus {
+            color: white;
+            text-shadow: 0 1px 1px #595959;
+            a {
+                margin-top: 1em;
+                font-size: 1.5em;
+                color: #fff;
+                &:hover {
+                    color: #22eeff;
+                }
+            }
+        }
+    }
+}
+</style>
+
+<template>
+    <header>
+        <div class="header-logo">
+            <a href="http://bugzhang.com/">
+                <span>
+                    <img class="header-logo-img"></img>
+                </span>
+            </a>
+        </div>
+        <div class="header-topic">
+            <h1>{{headerTopic}}</h1>
+            <h4>{{headerSign}}</h4>
+            <span class="header-menus">
+                <a :href="menu.url" v-for="menu in headerMenus">{{menu.text}}</a>
+            </span>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            headerTopic: 'Bugzhang的博客',
+            headerSign: 'Keep clam and carry on',
+            headerMenus:[{
+                text:'主页 | ',
+                url:'http://bugzhang.com/'
+            },{
+                text:'留言板 | ',
+                url:'http://bugzhang.com/'
+            },{
+                text:'好友链接 | ',
+                url:'http://bugzhang.com/'
+            },{
+                text:'关于我 | ',
+                url:'http://bugzhang.com/'
+            },{
+                text:'作品集',
+                url:'http://bugzhang.com/'
+            }]
+        }
+    },
+
+    methods: {
+        handleStart() {
+            this.$Modal.info({
+                title: 'Bravo',
+                content: 'Now, enjoy the convenience of iView.'
+            });
+        }
+    }
+};
+</script>
